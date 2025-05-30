a5492661fb1b6f6130539a2ae76b3b1728e65fee
chinmoybuzz <chinmoybuzz@gmail.com>
2025-05-31 00:30:20 +0530

    home page update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Home = () => {
   return (
     <div>
       <h1>Home page</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate nihil alias, aspernatur impedit magni animi. Amet qui nemo non reprehenderit vitae alias suscipit quasi assumenda. Maiores exercitationem quaerat fugit corporis.</p>
+
+      <Link to={"/about-us"}>
+        <button>About us</button>
+      </Link>
     </div>
   );
 };
